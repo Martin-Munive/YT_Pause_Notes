6de3e8b696cdfbc78913de3bb7a61e72be15a679
Martin-Munive <nestor.munive.a@gmail.com>
2024-03-31 22:48:05 -0500

    Ventana, 1 boton, 1 etiqueta. Interactivos.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-from tkinter import *
-root = Tk()
+import tkinter as tk
+root = tk.Tk()
+root.title("Youtube Stoper")
+root.configure(background="white")
+root.minsize(300, 100)
+root.maxsize(300, 100)
+
+
+def on_pause_button_click():
+    message = "Estado: youtube.pause_youtube()"
+    status_label.config(text=message)
+
+
+# Botón de pausa
+pause_button = tk.Button(root, text="Pausar YouTube",
+                         command=on_pause_button_click)
+pause_button.pack()
+
+# Etiqueta de estado
+status_label = tk.Label(
+    root, text="Estado: Esperando acción.", justify="center")
+status_label.pack()
+
 root.mainloop()
